decec2c427fdfb143d6b4304238f03ecee8f5839
mayoumelon <mayoumelon@gmail.com>
2021-01-17 07:11:10 +0900

    固定ページ用php新規作成（header/footerのみ）

--- functions.php
+++ functions.php
@@ -34,9 +34,9 @@ function my_script_init()
 
 wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', array(), '5.8.2', 'all');
 wp_enqueue_style('wow', get_template_directory_uri() . '/css/animate.css', array(), '1.0.0', 'all');
-wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
+wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css?var='.time(), array(), '1.0.0', 'all'); //time()キャッシュ対策らしい
 wp_enqueue_script('wow', get_template_directory_uri() . '/js/wow.min.js', array( 'jquery' ), '1.0.0', true);
-wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.0', true);
+wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js?var='.time(), array( 'jquery' ), '1.0.0', true);//time()キャッシュ対策らしい
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
