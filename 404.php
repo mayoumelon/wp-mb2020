2e285ad99728081a61b7bab75f3277333ce46cb2
mayoumelon <mayoumelon@gmail.com>
2021-01-29 06:04:17 +0900

    404 作成

---
+++ 404.php
@@ -0,0 +1,26 @@
+<!--ヘッダ -->
+<?php get_header(); ?>
+
+  <!-- メインここから-->
+  <main>
+    <div class="err__inner">
+      <section class="hp_smTopSpace">
+        <!-- タイトル-->
+        <h2 class="el_lv2Heading hp_smTopSpace">お探しのページはありません</h2>
+    
+        <!-- コンテンツここから-->
+        <div class="container">
+    
+          <!-- Contact -->
+          <div class="bl_inquiry hp_smTopSpace">
+            <p>申し訳ありませんが、お探しのページが存在しないか、アクセスできません。<br>入力されたURLが正しいかご確認ください。</p>
+          </div>
+        </div>
+        <!-- コンテンツここまで-->
+      </section>
+    </div><!-- /.err__inner -->
+  </main>
+  <!-- メインここまで  -->
+
+<!--フッタ -->
+<?php get_footer(); ?>
